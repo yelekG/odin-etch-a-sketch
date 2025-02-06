d52b3fba02fa6cc2a9885dedd1f8d6153d67e751
yelekG <155242981+yelekG@users.noreply.github.com>
2025-02-06 15:55:23 +0300

    Renkli hale getirildi.

--- script.js
+++ script.js
@@ -4,21 +4,43 @@ const newGridButton = document.getElementById("new-grid");
 function createGrid(size) {
     container.innerHTML = ""; //Her yeni grid oluşturduğumuzda eski kareleri temizler.
     
-    const squareSize = 500 / size;
+    const squareSize = Math.floor(960 / size);
 
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
-        square.classList.add("grid-square")
+        square.classList.add("grid-square");
 
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
 
+        square.style.backgroundColor = "rgba(255, 255, 255, 1)";
+        square.dataset.opacity = "0";
+
+
         square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "black";
+            let opacity = parseFloat(square.dataset.opacity);
+
+            if (opacity === 0) {
+                square.dataset.red = randomRGB();
+                square.dataset.green = randomRGB();
+                square.dataset.blue = randomRGB();
+            }
+
+            
+            if (opacity < 1) {
+                opacity += 0.1;
+                square.dataset.opacity = opacity.toString();
+                square.style.backgroundColor = `rgba(${square.dataset.red}, ${square.dataset.green}, ${square.dataset.blue}, ${opacity})`;
+            }
+            
         });
 
         container.appendChild(square);
     }
+}
+
+function randomRGB() {
+    return Math.floor(Math.random() * 256);
 }
 
 
